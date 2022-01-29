0c0da4e072260c92c35bc6f66b928ffe869888bc
xxFREESHROUDxx <baibhavkc11@gmail.com>
2022-01-30 00:32:23 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face"
+  }, {
+    emoji: "🎉",
+    name: "party pooper"
+  }, {
+    emoji: "💃",
+    name: "woman dancing"
+  }
+]
 
 function App() {
+  const greeting = "greeting";
+  const displayAction = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
+      {displayAction && <p>I am writing JSX</p>}
+      {/* <p>I am writing JSX</p> */}
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name} >
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+        {/* { <li>
+          <button onClick={greetings}>
+            <span role="img" aria-label="party popper" id="party popper">🎉</span>
+          </button>
+        </li>
+        <li>
+          <button onClick={greetings}>
+            <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+          </button>
+        </li> } */}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
